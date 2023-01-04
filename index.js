8330fbae7486a2a471b0b6a9354a0fd3b8a40a13
Dhiraj <dhirajswm15@gmail.com>
2023-01-04 23:02:40 +0530

    learned about scopes

--- index.js
+++ index.js
@@ -15,3 +15,46 @@
    //       }
    //    }
 
+// fn();
+
+// function fn(){
+//   console.log("i am real");
+// }
+// function fn(){
+//   console.log(" no i am real");
+// }
+// function fn(){
+//   console.log("i am real also");
+// }
+
+//  fn();
+
+// console.log("32",a);
+// var a=10;
+// function fn(){
+//   console.log("35",a);
+//   var a=20;
+//   console.log("37",a);
+// }
+// console.log("39",a);
+// fn();
+// console.log("41",a);
+
+//*************SCOPE*********
+
+var a=10;
+false
+function b(){
+  console.log('48',a);
+}
+
+function c(){
+  var a=20;
+  console.log("53",a);
+  b();
+  console.log("55",a);
+}
+
+console.log("58",a);
+c();
+console.log("60",a);
